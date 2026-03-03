b2c99f943665cc3fb8b2857363a74d32b9334541
김민지 <minkim9821@gmail.com>
2026-03-03 15:13:21 +0900

    function끝. 예제6이랑 dom남음

---
+++ ch0-ex/ex6/js/index@.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const btnMenu = document.querySelector(".btn-menu")
+    const btnMenuClose = document.querySelector(".btn-menu-close")
+    const hiddenGnb = document.querySelector(".hiddenGnb")
+})
---
+++ ch34-dom/js/study4.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded",function(){})
+
+
+const btn1 = document.querySelector("#btn1")
+btn1.addEventListener("click", function(){
+    alert("안녕하세요")
+})
+
+const btn2 = document.querySelector("#btn2")
+const box = document.querySelector(".box")
+btn2.addEventListener("click", function(){
+    box.style.backgroundColor = "orange"
+})
+
+
+const btnFontBase = document.querySelector("#btn-font-base")
+btnFontBase.addEventListener("click",()=>{
+    alert()
+})
