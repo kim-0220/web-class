c703ceebc5e0402806cf5cd34f7286f76690d30e
김민지 <minkim9821@gmail.com>
2026-03-05 02:21:35 +0900

    예제6 완료~~

--- ch0-ex/ex6/js/responsive index.js
+++ ch0-ex/ex6/js/responsive index.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded",function(){
+
     const btnMenu = document.querySelector(".btn-menu")
     const btnMenuClose = document.querySelector(".btn-menu-close")
     const hiddenGnb = document.querySelector(".hiddenGnb")
+
+    btnMenu.addEventListener("click",()=>{
+        hiddenGnb.classList.add("on")
+    })
+
+    btnMenuClose.addEventListener("click",()=>{
+        hiddenGnb.classList.remove("on")
+    })
 })
