8dddc02aa1e662d5049eabee7291a19c602a7947
김민지 <minkim9821@gmail.com>
2026-03-12 06:42:22 +0900

    am5:40~6:40

--- ch36-swiper-js-api/js/study1.js
+++ ch36-swiper-js-api/js/study1.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded",function(){
-    const swiper = new Swiper('.station', {
+
+    const station1 = new Swiper('.station', {
     // Optional parameters(선택적인 매개변수)
         direction: 'vertical',
         loop: true,
@@ -25,11 +26,11 @@ document.addEventListener("DOMContentLoaded",function(){
         },
     });
 
-    const swiper2 = new Swiper('.station2', {
+    const station2 = new Swiper('.station2', {
         direction: 'horizontal',
         loop: true,
-        spaceBetween: 40,
         speed: 300,
+        // 슬라이드가 실제로 움직이는 속도 0.3초(슬라이드가 움직이는 시간)
         pagination: {
             el: ".swiper-pagination",
             type: "progressbar",
@@ -39,10 +40,13 @@ document.addEventListener("DOMContentLoaded",function(){
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
-        centeredSlides: true,
         autoplay: {
             delay: 2500,
+            // 슬라이드가 자동으로 넘어가기전에 기다리는 시간 2.5초(2.5초 멈춰있다가 다음으로 넘어감)
             disableOnInteraction: false,
+            // 사용자가 슬라이드를 드래그하거나 화살표를 누르면 autoplay가 멈추는 기능(값이 true일때 - 기본값)
+            // 위의 경우 false이기 때문에 사용자가 건드려도 자동 슬라이드가 유지됨
         },
     });
+
 })
