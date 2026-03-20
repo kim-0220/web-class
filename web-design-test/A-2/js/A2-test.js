a8493a8c84873a0128e5259c4564eff1f4c0676a
김민지 <minkim9821@gmail.com>
2026-03-21 01:26:06 +0900

    test A/B 완료

--- web-design-test/A-2/js/A2-test.js
+++ web-design-test/A-2/js/A2-test.js
@@ -5,17 +5,17 @@ document.addEventListener("DOMContentLoaded",function(){
     const modal = document.querySelector(".modal")
     const btnClose = document.querySelector("#btn-close")
 
-    btnPopup.addEventListener("click", ()=>{
+    btnPopup.addEventListener("click",()=>{
         popup.classList.add("on")
         modal.classList.add("on")
     })
 
-    btnClose.addEventListener("click", ()=>{
+    btnClose.addEventListener("click",()=>{
         popup.classList.remove("on")
         modal.classList.remove("on")
     })
 
-    const train= document.querySelector(".train")
+    const train = document.querySelector(".train")
     let count = 0
 
     setInterval(()=>{
@@ -25,9 +25,15 @@ document.addEventListener("DOMContentLoaded",function(){
         if(count==3){
             setTimeout(()=>{
                 train.style.transition = `none`
+                // 애니메이션 없이 즉시 이동
                 train.style.transform = `translateX(0%)`
+                // 처음으로 순간이동
                 count = 0
+                // 다시 처음부터 시작
             },800)
+            // 0.8초인 이유는 transition이 0.7초라서, 즉 애니메이션이 끝나고 리셋하기 위함이다.
         }
     },2500)
+    // 정리하자면 2.5초마다 슬라이드가 이동하고, 0,1,2,3 순서로 이동하다가 3에서 멈춘 후 애니메이션 없이 0으로 순간 이동하며 다시 반복되는 구조다.
+    // 이 js가 없으면 마지막에서 첫번째 슬라이드로 다시 돌아갈때 튀는 애니메이션이 발생하며, 위처럼 했을 때 사용자 입장에서 무한 슬라이드처럼 자연스럽게 보인다.
 })
---
+++ web-design-test/B-2/js/B2-test.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    const btnPopup = document.querySelector("#btn-popup")
+    const popup = document.querySelector(".popup")
+    const modal = document.querySelector(".modal")
+    const btnClose = document.querySelector("#btn-close")
+
+    btnPopup.addEventListener("click",()=>{
+        modal.classList.add("on")
+        popup.classList.add("on")
+    })
+
+    btnClose.addEventListener("click",()=>{
+        modal.classList.remove("on")
+        popup.classList.remove("on")
+    })
+
+    const train = document.querySelector(".train")
+    let count = 0
+
+    setInterval(()=>{
+        count++
+        train.style.transition = `all 0.7s ease 0s`
+        train.style.transform = `translateY(${-25*count}%)`
+        if(count==3){
+            setTimeout(()=>{
+                count = 0
+                train.style.transition = `none`
+                train.style.transform = `translateY(0%)`
+            },800)
+        }
+    },2500)
+})
