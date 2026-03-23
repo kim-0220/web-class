169b906c1d4bd7731a0607a1d15a197da41bf032
김민지 <minkim9821@gmail.com>
2026-03-23 10:27:22 +0900

    다온차 database

---
+++ web-page-daoncha/js/listArray.js
@@ -0,0 +1,72 @@
+let listArray = [
+    {
+        pid:0,
+        // 상품의 고유번호 product id
+        pName:"딸기 화이트티",
+        // 상품명 product name
+        pDesc:"달콤한 딸기와 산뜻한 화이트 티가 어우러져 가볍고 부드러운 봄의 풍미를 느낄 수 있는 티",
+        // 상품 설명 product description
+        pPrice:4500,
+        // 상품 가격 product price
+        pThumbnail:"list-thumb-StrawberryWhiteTea.jpg",
+        // 상품 썸네일이미지 product thumbnail
+        plike:false
+        // 상품 좋아요버튼 product like
+    },
+    {
+        pid:1,
+        pName:"매화 꿀 티",
+        pDesc:"은은한 매화 향과 부드러운 꿀이 어우러져 따뜻하고 향긋한 봄의 여운을 전하는 티",
+        pPrice:4000,
+        pThumbnail:"list-thumb-PlumBlossomHoneyTea.jpg",
+        plike:false
+    },
+    {
+        pid:2,
+        pName:"복숭아 밀크티",
+        pDesc:"부드러운 밀크와 달콤한 복숭아 향이 어우러진 편안하고 사랑스러운 봄 메뉴",
+        pPrice:4600,
+        pThumbnail:"list-thumb-PeachMilkTea.jpg",
+        plike:false
+    },
+    {
+        pid:3,
+        pName:"깻잎 라떼",
+        pDesc:"한국의 허브, 깻잎을 현대적으로 재해석해 깻잎의 산뜻함과 부드러운 우유가 조화를 이루는 새로운 라떼",
+        pPrice:3800,
+        pThumbnail:"list-thumb-PerillaLatte.jpg",
+        plike:false
+    },
+    {
+        pid:4,
+        pName:"수박 화이트티",
+        pDesc:"달콤한 수박과 가벼운 화이트 티가 어우러진 맑고 청량한 여름 한 잔",
+        pPrice:4500,
+        pThumbnail:"list-thumb-WatermelonWhiteTea.jpg",
+        plike:false
+    },
+    {
+        pid:5,
+        pName:"매실 스파클링 티",
+        pDesc:"전통 매실의 산뜻함에 탄산을 더해 시원하게 즐기는 여름 티",
+        pPrice:4600,
+        pThumbnail:"list-thumb-PlumSparklingTea.jpg",
+        plike:false
+    },
+    {
+        pid:6,
+        pName:"자몽 허니 아이스티",
+        pDesc:"쌉쌀한 자몽과 부드러운 꿀이 어우러진 깊고 상큼한 여름 음료",
+        pPrice:4000,
+        pThumbnail:"list-thumb-GrapefruitHoneyIcedTea.jpg",
+        plike:false
+    },
+    {
+        pid:7,
+        pName:"누룽지 크림 라떼",
+        pDesc:"전통 누룽지의 깊고 구수한 풍미를 부드러운 크림으로 풀어낸 다온의 시그니처 라떼",
+        pPrice:3800,
+        pThumbnail:"list-thumb-NurungjiCreamLatte.jpg",
+        plike:false
+    }
+]
