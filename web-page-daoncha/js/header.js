897859da2041668a892ceb8b7fc2e595281c642f
김민지 <minkim9821@gmail.com>
2026-04-06 18:11:38 +0900

    다온 메인 헤더 sns 수정 조금 필요

--- web-page-daoncha/js/header.js
+++ web-page-daoncha/js/header.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     // 메뉴버튼을 누르면 header가 나오는 기능
     const btnMenu = document.querySelector(".btn-menu")
-    const menuSmartHidden = document.querySelector(".header-smart-hidden")
+    const headerSmartHidden = document.querySelector(".header-smart-hidden")
     const btnClose =  document.querySelector(".btn-close")
     const gnbSmartList = document.querySelectorAll(".gnb-smart>li")
 
     btnMenu.addEventListener("click",()=>{
-        menuSmartHidden.classList.add("on")
+        headerSmartHidden.classList.add("on")
     })
     btnClose.addEventListener("click",()=>{
-        menuSmartHidden.classList.add("on")
+        headerSmartHidden.classList.remove("on")
         gnbSmartList.forEach(tag=>tag.classList.remove("on"))
     })
 
