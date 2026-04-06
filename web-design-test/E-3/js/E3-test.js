a839ce8203122ec96c60166e1b740129533f9fb1
김민지 <minkim9821@gmail.com>
2026-04-06 23:38:13 +0900

    test C/E/F css랑 js 추가공부 필요~! 나머지는 완

---
+++ web-design-test/E-3/js/E3-test.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const btnPopup = document.querySelector("#btn-popup")
+    const popup = document.querySelector(".popup")
+    const btnClose = document.querySelector("#btn-close")
+
+    btnPopup.addEventListener('click',()=>{
+        popup.classList.add("on")
+    })
+    btnClose.addEventListener('click',()=>{
+        popup.classList.remove("on")
+    })
+
+    const train = document.querySelector(".train")
+    let count = 0
+
+    setInterval(()=>{
+        count++
+        if(count>2){count=0}
+        train.style.transform = `translateY(${-33.333*count}%)`
+    },2500)
+
+})
+
+// <!-- 다시 공부 필요 쌤꺼 복사함 ㅎㅋ -->
